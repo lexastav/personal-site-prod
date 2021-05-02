dba5cc7736a828a4709d7c71e98a6cf94ff05801
Alexey Volegov <lexastav@me.com>
2021-05-02 19:16:15 +0000

    Remove old stuff

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e-2cij+!ivn31b9!_-fi43d)x(2*p@ec3v22kzt=d9zinqq$%w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lexastav.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
